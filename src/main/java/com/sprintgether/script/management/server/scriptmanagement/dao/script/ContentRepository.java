47f3c9ee67798d960305a3b3f4088cf54182a842
azobs <ckiadjeu@gmail.com>
2021-10-27 05:35:29 +0200

    suite de la mise à jour du code sur la suppression de la classe ou du model Content qui est remplace par un siple string puisqu'au niveau de l'interface ce sera géré par un richTextEditor

--- src/main/java/com/sprintgether/script/management/server/scriptmanagement/dao/script/ContentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sprintgether.script.management.server.scriptmanagement.dao.script;
-
-import com.sprintgether.script.management.server.scriptmanagement.model.script.Content;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ContentRepository extends MongoRepository<Content, String> {
-}
